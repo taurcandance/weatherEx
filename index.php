ae197d4e006772705d2a5c7594d8b0c45ce94f4b
centr <centrcompstroi@mail.ru>
2018-11-14 03:03:41 +0300

    )

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once 'connectMemcachedServer.php';
+require_once 'connectMemcacheServer.php';
 
 $loader = new Twig_Loader_Filesystem('templates');
 $twig   = new Twig_Environment($loader);
